8527d3f0fb97b7637edb3b3dbbf0be79f69abf18
blangwell <b.langwell@outlook.com>
2020-11-08 12:05:05 -0800

    [index] Add logic to post route and test

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const route = express.Router();
 const port = process.env.PORT || 3000;
+
+// const route = express.Router();
+// app.use('/v1', route);
 
 const transporter = nodemailer.createTransport({
   port: 465,
@@ -20,23 +22,26 @@ const transporter = nodemailer.createTransport({
   secure: true,
 });
 
-route.post('/sendemail', (req, res) => {
+app.get('/sendemail', (req, res) => {
+  res.send('app hit');
+})
+
+app.post('/sendemail', (req, res) => {
   const mailData = {
     from: 'barentbetesting@gmail.com',
     to: 'barentbetesting@gmail.com',
-    subject: 'NODEMAILER TEST',
+    subject: 'SECOND TEST',
     text: 'holy shit',
-    html: '<h1> Holy shit! </h1>'
+    html: '<h1> Still workin! </h1>'
   };
   
-  transporter.sendMail(mailOptions, function (err, info) {
-    if (err) console.log(err)
-    else console.log(info)
+  transporter.sendMail(mailData, function (err, info) {
+    if (err) return console.log(err)
+    else return console.log(info)
   })
 
 })
 
 
-app.use('/v1', route);
 
 app.listen(port, () => console.log(`listenin' on port ${port}`))
