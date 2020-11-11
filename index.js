ca7f3a85cc8e5bc04fd743bff4f61eeb218b0cf2
blangwell <b.langwell@outlook.com>
2020-11-10 16:08:26 -0800

    [index] Replace raw api key with environment variable, change html to text in mailData object

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ require('dotenv').config()
 const bodyParser = require('body-parser');
 const cors = require('cors')
 const express = require('express');
+const fetch = require('node-fetch')
 const nodemailer = require('nodemailer');
 const port = process.env.PORT || 3000;
-const axios = require('axios');
-const fetch = require('node-fetch')
-const secret = process.env.RECAPTCHA_SECRET_KEY;
+const secretKey = process.env.RECAPTCHA_SECRET_KEY;
 
 const app = express();
 
+// middleware
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -29,23 +29,7 @@ transporter.verify(function(error, success) {
   else console.log('server ready to receive messages')
 })
 
-// const human = validateHuman(token);
-
-// if (!human) {
-//   res.status(400);
-//   rest.json('you fool us not, bot')
-//   return
-// }
-
-// async function validateHuman(token) {
-//   const response = await axios.post(`https://www.google.com/recaptcha/api/siteverify/secret=${secret}&response=${token}`)
-//   .then(response => {
-//     console.log(response, 'recaptcha response')
-//     response.json();
-//   })
-//   .catch(err => console.log(err))
-// }
-
+// send email post route
 app.post('/sendemail', async (req, res) => {
   const email = req.body.email;
   const message = req.body.message;
@@ -55,23 +39,15 @@ app.post('/sendemail', async (req, res) => {
     from: `${email}`,
     to: 'barentbetesting@gmail.com',
     subject: `Music 4 All - ${message.slice(0, 20)}...`,
-    // text: `text : ${message}`,
-    html: `<p>from : ${email}</p> <p>message: ${message}</p>`
+    text: `from: ${email}\nmessage : ${message}`,
   };
-
-  // const captchaVerified = await axios.post(`https://www.google.com/recaptcha/api/siteverify?secret=6LcWA-EZAAAAAEPAUMc-FlViwUce0y1ElFUW3OT0&response=${token}`)
-  // .then(response => {
-  //   console.log('CAPTCHA POST RESPONSE', response)
-  //   return response
-  // })
-  // .catch(err => console.log('ISSUE WITH POST TO GOOGLE ', err))
   
-  const captchaVerified = await fetch(`https://www.google.com/recaptcha/api/siteverify?secret=6LcWA-EZAAAAAEPAUMc-FlViwUce0y1ElFUW3OT0&response=${token}`, {
+  const captchaVerified = await fetch(`https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${token}`, {
     method: "POST"
   })
   .then(_res => _res.json())
-
-
+  
+  
   if (captchaVerified.success === true) {
     transporter.sendMail(mailData, function (err, info) {
       if (err) res.json({status: 'fail'})
@@ -84,3 +60,31 @@ app.post('/sendemail', async (req, res) => {
 })
 
 app.listen(port, () => console.log(`listenin' on port ${port}`))
+
+// const human = validateHuman(token);
+
+// if (!human) {
+  //   res.status(400);
+  //   rest.json('you fool us not, bot')
+  //   return
+  // }
+  
+  // async function validateHuman(token) {
+    //   const response = await axios.post(`https://www.google.com/recaptcha/api/siteverify/secret=${secret}&response=${token}`)
+    //   .then(response => {
+      //     console.log(response, 'recaptcha response')
+      //     response.json();
+      //   })
+      //   .catch(err => console.log(err))
+      // }
+      // const captchaVerified = await axios.post(`https://www.google.com/recaptcha/api/siteverify?secret=6LcWA-EZAAAAAEPAUMc-FlViwUce0y1ElFUW3OT0&response=${token}`)
+      // .then(response => {
+        //   console.log('CAPTCHA POST RESPONSE', response)
+        //   return response
+        // })
+        // .catch(err => console.log('ISSUE WITH POST TO GOOGLE ', err))
+        
+        // const captchaVerified = await axios.post(`https://www.google.com/recaptcha/api/siteverify?secret=6LcWA-EZAAAAAEPAUMc-FlViwUce0y1ElFUW3OT0&response=${token}`, {
+          // })
+          // .then(response => console.log('AXIOS POST RESPONSE ', response))
+          
